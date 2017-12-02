fd0bb6b9023f3e8444e7b7088f8cbac475d252b8
Rodrigo Badin <r.badin@gmail.com>
2017-12-02 12:59:30 -0200

    Init git

---
+++ Assets/Scripts/Temp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+	public float value;
+
+	void Start()
+	{
+		StartCoroutine("Tween");
+	}
+
+	IEnumerator Tween()
+	{
+		float start = -5;
+		float end = 5;
+		float duration = 3;
+		float time = 0;
+		while (time < duration)
+		{
+			yield return new WaitForEndOfFrame();
+			time = Mathf.Clamp(time + Time.deltaTime, 0, duration);
+			value = EasingEquations.EaseInOutQuad(start, end, (time / duration));
+			transform.localPosition = new Vector3(value, 0, 0);
+		}
+	}
+}
